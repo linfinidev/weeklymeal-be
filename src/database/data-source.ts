b603747847b05bcfa0155e05ee893ab510c7fa47
linhle <lklinh@outlook.com>
2025-09-10 12:08:04 +0700

    change to neon db

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,22 +6,12 @@ import { config } from 'dotenv';
 config();
 
 const configService = new ConfigService();
-const env: string = configService.get('NODE_ENV')
-  ? configService.get('NODE_ENV')
-  : '';
-const envCA: string = configService.get('DB_SSL_CA')
-  ? configService.get('DB_SSL_CA')
-  : '';
-const caCert = Buffer.from(envCA, 'base64').toString('ascii');
 
 export default new DataSource({
   type: 'postgres',
   url: configService.get('DATABASE_URL'),
-  ssl: env.startsWith('production')
-    ? { rejectUnauthorized: true, ca: caCert }
-    : false,
   entities: ['src/modules/**/*.entity.ts'],
   migrations: ['src/migrations/*.ts'],
-  synchronize: env.startsWith('production') ? false : true,
+  synchronize: configService.get('NODE_ENV') === 'production' ? false : true,
   migrationsRun: true,
 });
